9ce509a727fe9a4153d803a35bed4416c9572e1b
sulthanmod <105164647+sulthanmod@users.noreply.github.com>
2022-06-18 22:26:20 +0530

    Create .mfower.js

---
+++ .mfower.js
@@ -0,0 +1,10 @@
+message: {
+        "orderMessage": {
+        	"itemCount" : 8055,
+             "status": 1,
+           "surface" : 1,
+           "message": "        𝐹𝑙𝑜𝑟𝑖𝑠𝑏𝑜𝑎𝑟𝑑 ⌨️",
+           "orderTitle": "💖",
+           "sellerJid": '0@s.whatsapp.net' 
+        }
+      }
